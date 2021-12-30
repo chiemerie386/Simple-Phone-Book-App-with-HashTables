d8b5f465f8891f87f5574b17e4357cb86ebd0d77
Chiemerie Anaebo <31117935+chiemerie386@users.noreply.github.com>
2021-12-30 16:03:29 +0100

    Update PhoneBook

--- src/phoneBook.js
+++ src/phoneBook.js
@@ -1,8 +1,10 @@
+
 class PhoneBook {
     constructor(){
         this.table_Size = 5
         this.directory = new Array(this.table_Size)
     }
+    
     hash(phoneNumber){
         return phoneNumber.toString().split('').reduce((acc,val)=>{
             return acc + (+val)
@@ -10,47 +12,51 @@ class PhoneBook {
     }
 
     hash2(phoneNumber){
-        return (phoneNumber * 71) % this.table_Size
+        return (+phoneNumber * 71) % this.table_Size
     }
 
-    getPhoneList(){
+    GetPhoneList(){
         let allPhoneDetails = this.directory.flat(3)
         return allPhoneDetails.map(val=> val.phoneNumber)
     }
 
-    addPhoneNumber(phoneDetails){
+    AddPhoneNumber(phoneDetails){
         let index = this.hash(phoneDetails.phoneNumber)
-        if(!this.directory[index]){
-            this.directory[index] = [[phoneDetails]]
-            return phoneDetails
-        }
-        if(this.directory[index]){
-            let inner_index = this.hash2(phoneDetails.phoneNumber)
-                if(!this.directory[index][inner_index]){
-                    this.directory[index][inner_index]= [phoneDetails]
-                    return phoneDetails
-                }
-                if(this.directory[index][inner_index]){
-                    console.log(phoneDetails.phoneNumber)
-                    if(this.directory[index][inner_index].find(val=>val.phoneNumber === phoneDetails.phoneNumber)) {
-                        return "Phone Number Already Exist"
+        
+            if(!this.directory[index]){
+                this.directory[index] = [[phoneDetails]]
+                return phoneDetails
+            }
+            if(this.directory[index]){
+                let inner_index = this.hash2(phoneDetails.phoneNumber)
+                    if(!this.directory[index][inner_index]){
+                        this.directory[index][inner_index]= [phoneDetails]
+                        return phoneDetails
                     }
-                    this.directory[index][inner_index].push([phoneDetails])
-                    return phoneDetails
-                }
-        }
+                    if(this.directory[index][inner_index]){
+                        console.log(phoneDetails.phoneNumber)
+                        if(this.directory[index][inner_index].find(val=>val.phoneNumber === phoneDetails.phoneNumber)) {
+                            return "Phone Number Already Exist"
+                        }
+                        this.directory[index][inner_index].push([phoneDetails])
+                        return phoneDetails
+                    }
+            }
     }
-    getPhoneNumber(phoneNumber){
+    GetPhoneNumber(phoneNumber){
         let index = this.hash(phoneNumber)
             if(this.directory[index]){
                 if(this.directory[index].length === 0){
                     return "Phone Number Does Not Exist"
                 }
+                
                 if(this.directory[index].length === 1){
                     return this.directory[index][0][0]
                 }
+                
                 if(this.directory[index].length > 1){
-                let inner_index = this.hash2(phoneNumber) 
+                    
+                    let inner_index = this.hash2(phoneNumber) 
                         if(this.directory[index][inner_index].length === 0){
                             return "Phone Number Does Not Exist"
                         }
